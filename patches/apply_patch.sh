63617d3516bb880c17f21d2072fe2b936f33846d
chenmudan <chenmudan@huawei.com>
2022-10-08 21:04:57 +0800

    support for gn updating Signed-off-by: chenmudan <chenmudan@huawei.com>
    Change-Id: I55974d84828f99898ad00b1c217ec1fbf23d6bf6

---
+++ patches/apply_patch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ../
+git apply patches/fd9f2036f26d83f9fcfe93042fb952e5a7fe2167.patch
+git apply patches/46b572ce4ceedfe57f4f84051bd7da624c98bf01.patch
+git apply patches/6f13aaac55a977e1948910942675c69f2b4f7a94.patch
