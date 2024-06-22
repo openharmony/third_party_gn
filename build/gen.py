10f3b402053e5c9260d18c2b8853fe067b23ed82
liangxinyan <liangxinyan2@huawei.com>
2024-06-22 16:23:54 +0800

    IssueNo: https://gitee.com/openharmony/third_party_gn/issues/IA7GIL Signed-off-by: liangxinyan <liangxinyan2@huawei.com>

--- build/gen.py
+++ build/gen.py
@@ -471,7 +471,7 @@ def WriteGNNinja(path, platform, host, options, args_list):
         '-Wno-unused-parameter',
         '-Wextra-semi',
         '-Wundef',
-        '-std=c++20'
+        '-std=c++17'
     ])
     
     if is_gcc(cxx):
@@ -489,7 +489,7 @@ def WriteGNNinja(path, platform, host, options, args_list):
         ldflags.append('-static-libstdc++')
 
       if platform.is_mingw() or platform.is_msys():
-        cflags.remove('-std=c++20')
+        cflags.remove('-std=c++17')
         cflags.extend([
           '-Wno-deprecated-copy',
           '-Wno-implicit-fallthrough',
@@ -498,7 +498,7 @@ def WriteGNNinja(path, platform, host, options, args_list):
           '-Wno-format',             # Use of %llx, which is supported by _UCRT, false positive
           '-Wno-strict-aliasing',    # Dereferencing punned pointer
           '-Wno-cast-function-type', # Casting FARPROC to RegDeleteKeyExPtr
-          '-std=gnu++20',
+          '-std=gnu++17',
         ])
       else:
         # This is needed by libc++.
@@ -570,7 +570,7 @@ def WriteGNNinja(path, platform, host, options, args_list):
         '/wd4505',
         '/wd4838',
         '/wd4996',
-        '/std:c++20',
+        '/std:c++17',
         '/GR-',
         '/D_HAS_EXCEPTIONS=0',
     ])
