b6090aa038cfa7908da5b16e228bd381a317a520
ring_king <1300547600@qq.com>
2024-09-05 14:26:04 +0000

    fix indep build bug
    Signed-off-by: ring_king <1300547600@qq.com>

--- src/gn/ohos_components_mapping.cc
+++ src/gn/ohos_components_mapping.cc
@@ -30,9 +30,18 @@ OhosComponentMapping *OhosComponentMapping::instance_ = nullptr;
 
 static std::map<std::string, std::string> gni_mapping_file_map_;
 
-static bool StartWith(const std::string &str, const std::string prefix)
+static bool StartWith(const std::string &str, const std::string &prefix)
 {
     return (str.rfind(prefix, 0) == 0);
+}
+
+const static std::string GetRealImportFile(const BuildSettings *settings, const std::string &path)
+{
+    base::FilePath file = base::FilePath(settings->root_path().MaybeAsASCII() + path);
+    if (!base::PathExists(file)) {
+        return "";
+    }
+    return path;
 }
 
 static bool ReadBuildConfigFile(base::FilePath path, std::string &content)
@@ -48,6 +57,7 @@ static void LoadGniMappingFileMap(const base::Value &value)
     for (auto info : value.DictItems()) {
         gni_mapping_file_map_[info.first] = info.second.GetString();
     }
+    return;
 }
 
 static std::map<std::string, std::function<void(const base::Value &value)>> mapping_map_ = {
@@ -82,17 +92,18 @@ OhosComponentMapping::OhosComponentMapping(const std::string &build_dir)
 {
     build_dir_ = build_dir;
     LoadMappingFile(build_dir);
+    return;
 }
 
-const std::string OhosComponentMapping::MappingTargetAbsoluteDpes(const BuildSettings* build_settings,
-    const std::string label, const std::string deps) const
+const std::string OhosComponentMapping::MappingTargetAbsoluteDpes(const BuildSettings *settings,
+    const std::string &label, const std::string &deps) const
 {
-    if (build_settings == nullptr || StartWith(deps, "//build/") ||
+    if (settings == nullptr || StartWith(deps, "//build/") ||
         StartWith(deps, "//out/")  || StartWith(deps, "//prebuilts/")) {
         return "";
     }
 
-    const OhosComponent *component = build_settings->GetOhosComponent(label);
+    const OhosComponent *component = settings->GetOhosComponent(label);
     if (component == nullptr) {
         return "";
     }
@@ -105,7 +116,7 @@ const std::string OhosComponentMapping::MappingTargetAbsoluteDpes(const BuildSet
     if (tool_sep != std::string::npos) {
         deps_without_tool = deps.substr(0, tool_sep);
     }
-    const OhosComponent *deps_component = build_settings->GetOhosComponent(deps_without_tool);
+    const OhosComponent *deps_component = settings->GetOhosComponent(deps_without_tool);
     if (deps_component == nullptr) {
         return "";
     }
@@ -117,19 +128,19 @@ const std::string OhosComponentMapping::MappingTargetAbsoluteDpes(const BuildSet
     return deps_component->getInnerApi(deps.substr(pos + 1));
 }
 
-const std::string OhosComponentMapping::MappingImportOther(const BuildSettings* build_settings,
-    const std::string label, const std::string deps) const
+const std::string OhosComponentMapping::MappingImportOther(const BuildSettings *settings,
+    const std::string &label, const std::string &deps) const
 {
-    if (build_settings == nullptr || StartWith(deps, "//build/") ||
+    if (settings == nullptr || StartWith(deps, "//build/") ||
         StartWith(deps, "//out/")  || StartWith(deps, "//prebuilts/")) {
         return "";
     }
-    const OhosComponent *component = build_settings->GetOhosComponent(label);
+    const OhosComponent *component = settings->GetOhosComponent(label);
     if (component == nullptr) {
         return "";
     }
     if (StartWith(deps, component->path())) {
         return "";
     }
-    return gni_mapping_file_map_[deps];
+    return GetRealImportFile(settings, gni_mapping_file_map_[deps]);
 }
